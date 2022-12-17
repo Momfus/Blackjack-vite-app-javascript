249bdb2ed3ce1be0d3f6b4c7b26d153b54c2270c
Momfus <momfus@outlook.com>
2022-12-17 01:38:40 -0300

    blackjack v1.1.0

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+
+// Configuración usada fuerta de despliegue de vite (como en github pages) y se configura aca las rutas para los recursos
+// Al constuir el proyecton con npm run build, se usa estas configuraciones (como agregar plugins, varibles de entorno, etc). Más en la documentación de vite.
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+   base: '/Blackjack-vite-app-javascript/'
+});
